54f41eb232fb659c40f03e0c141b134fb1391a32
Calin Buzetelu <calinbuze@gmail.com>
2023-05-27 15:06:04 +0200

    Add ExpensesFilter component
     - component which is used to show monetary impact of expenses based on a selected year

--- src/components/expenses/ExpenseList.js
+++ src/components/expenses/ExpenseList.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
+import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 import Card from "../ui/Card";
 import ExpenseItem from "./ExpenseItem";
 import './ExpenseList.css';
 
 function ExpenseList(props) {
+
+    const [filteredYear, setFilteredYear] = useState('2020')
+
+    const saveYearHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
+
     return (
-        <Card className="expenses">
-            <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
-            <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
-            <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
-            <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} />
-        </Card >
+        <div>
+            <ExpensesFilter selected={filteredYear} onYearChange={saveYearHandler} />
+            <Card className="expenses">
+                <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
+                <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
+                <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
+                <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} />
+            </Card >
+        </div>
     );
 }
 
